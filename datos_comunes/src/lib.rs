870c3a79dfb06e1bd1f6c67db3d413dc327d3697
L4Vo5 <l4vo5finalboss@gmail.com>
2024-05-03 20:41:35 -0300

    registrar front end TEST y encima no anda la respuesta (pero sí se registra)

---
+++ datos_comunes/src/lib.rs
@@ -0,0 +1,22 @@
+use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueryRegistrarUsuario {
+    pub nombre_y_apellido: String,
+    pub dni: u64,
+    pub email: String,
+    pub contraseña: String,
+    pub nacimiento: DateTime<Local>,
+}
+
+
+pub type ResponseRegistrarUsuario = Result<(), CrearUsuarioError>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum CrearUsuarioError {
+    ErrorIndeterminado,
+    DNIExistente,
+    EmailExistente,
+    MenorA18,
+}
