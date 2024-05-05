2b9cec2699702db4c214bdc27c91676b0fedfd76
nicobeiser <nbeisermann@gmail.com>
2024-05-05 12:57:17 -0300

    LOGRAMOS PERSISTENCIA DE DATOS!!!

--- frontend/src/Pages/profile_page.rs
+++ frontend/src/Pages/profile_page.rs
@@ -1,10 +1,19 @@
 use yew::prelude::*;
+use yewdux::prelude::*;
+use crate::store::UserStore;
 
 #[function_component(ProfilePage)]
 pub fn profile_page() -> Html {
+
+
+    let (store, dispatch) = use_store::<UserStore>();
+    let username = store.user.clone();
+
     html! (
         <>
             <h1>{"PROFILE"}</h1>
+            <div>{"your username is: "} {username}</div>
+
         </>
     )
 }
