55bcd5fd946fd8ce9a228b765e771c2aa0ce78bf
LucaGiordano <lucagior22@gmail.com>
2024-05-09 15:44:33 -0300

    Create Publication frontend listo

--- frontend/src/Molecules/create_publication_molecule.rs
+++ frontend/src/Molecules/create_publication_molecule.rs
@@ -1,75 +1,81 @@
-use std::vec;
-
-use crate::Components::{generic_input_field::GenericInputField, generic_button::GenericButton};
-use wasm_bindgen_futures::spawn_local;
-use yew::prelude::*;
-use web_sys::{FileReader, HtmlInputElement};
+use crate::Components::{generic_button::GenericButton, generic_input_field::GenericInputField};
 use wasm_bindgen::closure::Closure;
 use wasm_bindgen::JsCast;
+use web_sys::{FileReader, HtmlInputElement};
+use yew::prelude::*;
 
 #[function_component(CreatePublicationMolecule)]
 pub fn create_publication_molecule() -> Html {
-
-
-    
-    let title_state = use_state(|| "No title yet".to_owned());
-    let cloned_title_state = title_state.clone();
-    let title_changed = Callback::from(move |title:String|{
-        cloned_title_state.set(title.clone());
-    });
-    
-    let description_state = use_state(|| "No description yet".to_owned());
-    let cloned_description_state = description_state.clone();
-    let description_changed = Callback::from(move |description:String|{
-        cloned_description_state.set(description.clone());
-    });
-    
-    let submit_clicked = Callback::from(move |()| {
-        ()
-    });
-    
-    let onsubmit = Callback::from(move |event:SubmitEvent|{
-        event.prevent_default();
-    });
-    
-    let image_path = use_state(|| "".to_string());
-    let image_path_clone = image_path.clone();
-    
-    let image_list = use_state(|| Vec::new());
-    let image_list_clone = image_list.clone();
-
-    let oninput = move |event : InputEvent| {
-        event.prevent_default();
-        
-        let image_path_clone = image_path_clone.clone();
-        
-        let image_list_clone = image_list_clone.clone();
-        
-        let data_transfer : HtmlInputElement = event.target_dyn_into().unwrap();
-        
-        let file_list = data_transfer.files().unwrap();
-        
-        let file = file_list.get(0).unwrap();
-        
-        let reader = FileReader::new().unwrap();
-        
-        let reader_clone = reader.clone();
-        let onloaded = Closure::once_into_js(move || {
-            let path = reader_clone.result().unwrap().as_string().unwrap();
-            image_path_clone.set(path);
-            let new_vec = &*image_list_clone;
-            let mut mutable_vec = new_vec.clone();
-            let image_path_clone = image_path_clone.clone();
-            mutable_vec.push(html!(<img src={(&*image_path_clone).to_string()} height="200px" width="300px"/>));
-            image_list_clone.set(mutable_vec);
-        });
-        reader.set_onload(Some(onloaded.as_ref().unchecked_ref()));
-        
-        reader.read_as_data_url(&file).unwrap();
-        
+    let title_state = use_state(|| "".to_owned());
+    let title_changed = {
+        let title_state = title_state.clone();
+        Callback::from(move |title: String| title_state.set(title))
     };
-    
-    
+
+    let description_state = use_state(|| "".to_owned());
+    let description_changed = {
+        let description_state = description_state.clone();
+        Callback::from(move |description: String| description_state.set(description))
+    };
+
+    let onsubmit = Callback::from(move |event: SubmitEvent| {
+        event.prevent_default();
+        log::info!("Form submitted!")
+    });
+
+    let image_path = use_state(|| None);
+    let image_list = use_state(Vec::new);
+
+    let oninput = {
+        let image_path = image_path.clone();
+        let image_list = image_list.clone();
+        Callback::from(move |event: InputEvent| {
+            event.prevent_default();
+            let input: HtmlInputElement = event.target_dyn_into().unwrap();
+            let file_list = input.files().unwrap();
+            input.set_disabled(true);
+
+            let file = file_list.get(0).unwrap();
+
+            let reader = FileReader::new().unwrap();
+
+            let onload = {
+                let image_path = image_path.clone();
+                let image_list = image_list.clone();
+                let reader = reader.clone(); // Clona el FileReader si es posible (FileReader no implementa Clone, ver nota abajo)
+                Closure::once_into_js(Box::new(move || {
+                    if let Ok(result) = reader.result() {
+                        if let Some(url) = result.as_string() {
+                            image_path.set(Some(url.clone()));
+                            image_list.set({
+                                let mut list = (*image_list).clone();
+                                list.push(html! { <img src={url} height="200px" width="300px"/> });
+                                list
+                            });
+                        }
+                    }
+                    input.set_disabled(false);
+                }) as Box<dyn FnMut()>)
+            };
+
+            reader.set_onload(Some(onload.as_ref().unchecked_ref()));
+            reader.read_as_data_url(&file).unwrap();
+        })
+    };
+
+    let delete_last_image = {
+        let image_path = image_path.clone();
+        let image_list = image_list.clone();
+        Callback::from(move |event: MouseEvent| {
+            image_list.set({
+                let mut list = (*image_list).clone();
+                list.pop();
+                list
+            });
+            log::info!("{:?}", (&*image_list))
+        })
+    };
+
     html!(
         <div class="create-publication-box">
             <form {onsubmit}>
@@ -79,20 +85,25 @@ pub fn create_publication_molecule() -> Html {
                     <GenericInputField name="Descripción" label="Ingrese una descripción para la publicación" tipo="text" handle_on_change={description_changed}/>
                 </div>
                 <div class="image-prompts">
-                    <input oninput={oninput} type="file" id="file" name="publication_img" multiple={true} accept=".png, .jpg, .jpeg"/>
+                    <input oninput={oninput} type="file" id="file" name="publication_img" multiple={true}/>
                 </div>
                 <div class="image-preview">
                     if !(&*image_list).is_empty() {
                         <h2>{"Aqui se previsualizan tus imágenes:"}</h2>
-                        <ul>
-                            {(&*image_list).iter().map(|image| html!(<li>{(&*image).clone()}</li>)).collect::<Html>()}
+                        <ul class="image-list">
+                            {(&*image_list).iter().map(|image| html!(<li class="image-item">{(&*image).clone()}</li>)).collect::<Html>()}
                         </ul>
+                        <button type="button" onclick={delete_last_image}>{"Eliminar última imagen"}</button>
                     }
                 </div>
                 <div class="submit_button">
-                    <GenericButton text="Publicar" onclick_event={submit_clicked}/>
+                    if !((&*title_state).is_empty()) && !((&*description_state).is_empty()) && !((&*image_list).is_empty()) {
+                        <input type="submit" value="Confirmar"/>
+                    } else {
+                        <button class="disabled-dyn-element">{"Confirmar"}</button>
+                    }
                 </div>
             </form>
         </div>
-    )
+)
 }
