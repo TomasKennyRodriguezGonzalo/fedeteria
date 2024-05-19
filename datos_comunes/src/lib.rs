2e4a451fbde50d641ab2955497db27beb57c1e58
LucaGiordano <lucagior22@gmail.com>
2024-05-19 00:23:43 -0300

    Publication grid frontend listo

--- datos_comunes/src/lib.rs
+++ datos_comunes/src/lib.rs
@@ -14,29 +14,6 @@ pub struct QueryRegistrarUsuario {
     pub nacimiento: DateTime<Local>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Publicacion {
-    pub dni_usuario: u64,
-    pub titulo: String,
-    pub descripcion: String,
-    pub imagenes: Vec<String>,
-    pub precio: Option<u64>,
-    pub pausada: bool,
-}
-
-impl Publicacion {
-    pub fn new(titulo: String, descripcion: String, imagenes: Vec<String>, dni_usuario: u64) -> Self {
-        Publicacion { 
-            titulo,
-            descripcion, 
-            imagenes,
-            dni_usuario,
-            precio: None,
-            pausada: true,
-        }
-    }
-}
-
 pub type ResponseRegistrarUsuario = Result<(), CrearUsuarioError>;
 
 #[derive(Debug, Serialize, Deserialize)]
